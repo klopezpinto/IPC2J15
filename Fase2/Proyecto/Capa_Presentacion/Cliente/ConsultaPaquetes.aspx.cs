cd9fc3e1503c655f07c51fa9d2e2f4dee7ffba93
Karen López <klopezpinto@gmail.com>
2015-06-18 19:56:38 -0600

    Fase 2
    Cliente

--- Fase2/Proyecto/Capa_Presentacion/Cliente/ConsultaPaquetes.aspx.cs
+++ Fase2/Proyecto/Capa_Presentacion/Cliente/ConsultaPaquetes.aspx.cs
@@ -15,30 +15,11 @@ public partial class Cliente_ConsultaPaquetes : System.Web.UI.Page
     {
         if (e.CommandName == "Select")
         {
-            //
-            // Se obtiene indice de la row seleccionada
-            //
             int index = Convert.ToInt32(e.CommandArgument);
-
-            //
-            // Obtengo el id de la entidad que se esta editando
-            // en este caso de la entidad Person
-            //
             int id = Convert.ToInt32(GridView1.DataKeys[index].Value);
-            Label34.Text = id.ToString();
+            Session["ValorKey"] = id.ToString();
 
+            Response.Redirect("PaqueteIndividual.aspx");
         }
-
-    }
-    protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        //    GridViewRow row = GridView1.SelectedRow;
-
-        //    //
-        //    // Obtengo el id de la entidad que se esta editando
-        //    // en este caso de la entidad Person
-        //    //
-        //    int id = Convert.ToInt32(GridView1.DataKeys[row.RowIndex].Value);
-        //}
     }
 }
---
+++ Fase2/Proyecto/Capa_Presentacion/Cliente/PaqueteIndividual.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Cliente/MasterPage.master" AutoEventWireup="true" CodeFile="PaqueteIndividual.aspx.cs" Inherits="Cliente_PaqueteIndividual" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder2" Runat="Server">
+    <asp:Label ID="Label33" runat="server" Style="text-align: right" Text="Usuario:"></asp:Label>
+    &nbsp;<asp:Label ID="Label32" runat="server" Style="text-align: right" Text="Label"></asp:Label>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:Label ID="Label34" runat="server" Text="Label"></asp:Label>
+</asp:Content>
+
